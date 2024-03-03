29d6f97553ca142b7d5809ab1555f3c66e6e98b9
mohamed <zmuhamedosama@gmail.com>
2024-03-03 08:16:45 +0200

    morning thinking

--- playground.js
+++ playground.js
@@ -15,16 +15,16 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-var twoSum = function (nums, target) {
-  for (let index = 0; index < nums.length; index++) {
-    const diff = target - nums[index];
-    const diffIndex = nums.indexOf(diff);
-    // "diffIndex !== index" takes care of same index not being reused
-    if (diffIndex !== -1 && diffIndex !== index) {
-      return [index, diffIndex];
-    }
-  }
-};
+// var twoSum = function (nums, target) {
+//   for (let index = 0; index < nums.length; index++) {
+//     const diff = target - nums[index];
+//     const diffIndex = nums.indexOf(diff);
+//     // "diffIndex !== index" takes care of same index not being reused
+//     if (diffIndex !== -1 && diffIndex !== index) {
+//       return [index, diffIndex];
+//     }
+//   }
+// };
 
 // var twoSum = function (nums, target) {
 //   let initialIndex = nums[0];
@@ -42,4 +42,21 @@ var twoSum = function (nums, target) {
 //   }
 // };
 
-console.log(twoSum([1, 2, 5, 2, 57, 24, 3, 4], 7));
+// 2. Add Two Numbers
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+var addTwoNumbers = function (l1, l2) {
+  let res;
+  if (Array.isArray(l1) && Array.isArray(l2)) {
+    let num1 = Number(l1.reverse().join(""));
+    let num2 = Number(l2.reverse().join(""));
+
+    let sum = num1 + num2;
+    let sumArr = sum.toString().split("").reverse();
+    res = sumArr;
+  }
+  return res;
+};
+
+console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
